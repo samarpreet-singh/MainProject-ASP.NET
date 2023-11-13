968675fe8063396049beb894120386f72d8be605
Samarpreet Singh <samarpreet.singh@mygeorgian.ca>
2023-11-13 00:59:07 -0500

    Scaffold all reqd pages, styled all reqd pages, add QR code authentication, navbar styling added

--- Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
@@ -180,7 +180,7 @@ namespace MainProject.Areas.Identity.Pages.Account.Manage
             return string.Format(
                 CultureInfo.InvariantCulture,
                 AuthenticatorUriFormat,
-                _urlEncoder.Encode("Microsoft.AspNetCore.Identity.UI"),
+                _urlEncoder.Encode("Crafty Creations"),
                 _urlEncoder.Encode(email),
                 unformattedKey);
         }
